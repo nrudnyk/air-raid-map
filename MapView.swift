7072f30c4e1529c10c34a26a8429b5b886ba3c2f
nrudnyk <nazar@scichart.com>
2022-04-23 19:57:01 +0300

    style: cleanup MapView preview

--- MapView.swift
+++ MapView.swift
@@ -12,20 +12,16 @@ struct MapView: View {
     @StateObject private var viewModel = MapViewModel()
     
     var body: some View {
-        
-//        SwiftUIMapView.MapView(region: $viewModel.region)
-//        Map(coordinateRegion: $viewModel.region)
-        MapViewRepresentable(region: $viewModel.region, overlays: viewModel.overlays)
+        MapViewRepresentable(
+            region: $viewModel.region,
+            overlays: viewModel.overlays
+        )
             .ignoresSafeArea()
     }
 }
 
-//struct MapView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MapViewRepresentable(
-//            region: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D.centerOfUkraine, span: MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2))),
-//            overlays: .constant([])
-//        )
-//            .ignoresSafeArea()
-//    }
-//}
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
