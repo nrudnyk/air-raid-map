280a6a165fcbde179387b0361c392f4aaa8d405c
nrudnyk <nazar@scichart.com>
2022-04-27 16:45:54 +0300

    feat: increased Ukraine bounding box

--- Model/MapContants.swift
+++ Model/MapContants.swift
@@ -11,6 +11,6 @@ public enum MapConstsants {
     public static let centerOfUkraine = CLLocationCoordinate2DMake(48.84964001422709, 31.18144598789513)
     public static let boundsOfUkraine = MKCoordinateRegion(
         center: centerOfUkraine,
-        span: MKCoordinateSpan(latitudeDelta: 7.327285995706916, longitudeDelta: 18.0914518609643)
+        span: MKCoordinateSpan(latitudeDelta: 7.327285995706916 + 1, longitudeDelta: 18.0914518609643 + 1)
     )
 }
